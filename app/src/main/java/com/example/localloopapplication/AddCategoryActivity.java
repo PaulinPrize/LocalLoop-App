6cdd6435e34c4953dc9f3ba3d06674c0b474a5d4
ElectricByFrancko <francknana79@yahoo.com>
2025-06-15 08:14:04 -0400

    Updated Strings file

--- app/src/main/java/com/example/localloopapplication/AddCategoryActivity.java
+++ app/src/main/java/com/example/localloopapplication/AddCategoryActivity.java
@@ -24,7 +24,7 @@ public class AddCategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_category_add);
 
-        etCategoryName = findViewById(R.id.categoryTextView);
+        etCategoryName = findViewById(R.id.categoryTextViewMenu);
         etCategoryDescription = findViewById(R.id.etCategoryDescription);
         btnSubmit = findViewById(R.id.btnSubmitCategory);
         categoryRef = FirebaseDatabase.getInstance().getReference("event_categories");
